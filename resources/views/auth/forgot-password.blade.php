1dd20857c84bfe12941828fd365e81037c5479fc
Mibadd <mhmmdibad22@gmail,com>
2025-07-18 15:23:57 +0700

    Final Authentication

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,17 +1,17 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Masukan Email Anda Yang Terdaftar Untuk Verifikasi.') }}
+        Masukkan email Anda yang terdaftar untuk verifikasi dan mendapatkan tautan reset kata sandi.
     </div>
 
-    <!-- Session Status -->
+    <!-- Status Sesi -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email Address -->
+        <!-- Alamat Email -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="'Email'" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
                 autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
@@ -19,7 +19,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                Kirim Tautan Reset Kata Sandi
             </x-primary-button>
         </div>
     </form>
